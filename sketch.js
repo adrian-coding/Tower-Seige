aa89cff924bedb1b175d8c2972f5240726ae788b
adrian-coding <70213677+adrian-coding@users.noreply.github.com>
2020-11-30 20:22:37 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var a;
+var circles=[];
+function setup() {
+  createCanvas(800,800);
+  stroke(255)
+  
+ 
+  a=height;
+  circles.push(width/2)
+}
+
+function draw() {
+  //camera.zoom=camera.zoom+1
+  background(0);  
+  
+  
+  a=a-1;
+  
+ 
+  
+  for (i=0;i<circles.length;i++)
+{
+	circle(circles[i], height/2,20)
+}
+if(camera.position.x%width===0)
+{
+	circles.push(camera.position.x+width/2)
+}
+ //camera(0, 0, 20 + sin(frameCount * 0.01) * 10, 0, 0, 0, 0, 1, 0);
+ drawSprites();
+}
+
+function keyPressed ()
+{
+  if(keyCode === RIGHT_ARROW)
+  {
+  }
+} 
